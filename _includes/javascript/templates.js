0398524adee5022953dd2163c85b2fdc3cf61659
peteredwards <p.l.edwards@leeds.ac.uk>
2024-01-08 16:36:04 +0000

    Updated capacity and occupancy messages

--- _includes/javascript/templates.js
+++ _includes/javascript/templates.js
@@ -196,8 +196,9 @@ function updateOccupancy() {
 						if ( pco > 100 ) {
 							pco = 100;
                         }
-                        let occupancyMsg = spacefinder.occupancyData[lib].occupancy < 50? "less than 50": spacefinder.occupancyData[lib].occupancy;
-                        sdo.innerHTML = 'There are currently <strong>'+occupancyMsg+'</strong> people in the <strong>'+lib+' library</strong>, which has a seating capacity of approximately <strong>'+spacefinder.occupancyData[lib].capacity+'</strong>';
+                        let occupancyMsg = spacefinder.occupancyData[lib].occupancy < 50? "less than 50": spacefinder.occupancyData[lib].occupancy.toLocaleString('en');
+                        let capacityMsg = spacefinder.occupancyData[lib].capacity.toLocaleString('en');
+                        sdo.innerHTML = 'There are currently <strong>'+occupancyMsg+'</strong> people in the <strong>'+lib+' library</strong>, which has a seating capacity of approximately <strong>'+capacityMsg+'</strong>';
 					});
 				} else {
                     splog("No occupancy data for "+lib);
@@ -227,7 +228,9 @@ function updateSpaceInfoWindowContent() {
                     let content = '<div class="spaceInfoWindow"><h3>'+spacefinder.spaces[i].title+'</h3>';
                     content += '<p class="info">' + info.join(', ') + '</p>';
                     content += '<p class="description">' + spacefinder.spaces[i].description + '</p>';
-                    content += '<p class="occupancy icon-user">There are currently <strong>'+spacefinder.occupancyData[lib].occupancy+'</strong> people in the <strong>'+lib+' library</strong>, which has a seating capacity of approximately <strong>'+spacefinder.occupancyData[lib].capacity+'</strong></p>';
+                    let occupancyMsg = spacefinder.occupancyData[lib].occupancy < 50? "less than 50": spacefinder.occupancyData[lib].occupancy.toLocaleString('en');
+                    let capacityMsg = spacefinder.occupancyData[lib].capacity.toLocaleString('en');
+                    content += '<p class="occupancy icon-user">There are currently <strong>'+occupancyMsg+'</strong> people in the <strong>'+lib+' library</strong>, which has a seating capacity of approximately <strong>'+capacityMsg+'</strong></p>';
                     content += '<button class="show-list">More info&hellip;</button></div>';
                     spacefinder.spaces[i].marker.setPopupContent( content );
                 }
